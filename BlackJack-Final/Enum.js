a3f6f09220aaded25748ac14c86747b45baf6269
Michael Davis <rubiksmaster65@yahoo.com>
2016-11-30 16:33:18 -0500

    Major overhaul of code
    Simplifications of variables and clearing the global namespace with an
    enum file which contains all constants and flexible values

---
+++ BlackJack-Final/Enum.js
@@ -0,0 +1,37 @@
+var Const = Object.freeze({
+	deckSize : 52,
+	humanElement : 4,
+	sliderMax : 10,
+	cardSpacing : 20,
+	maxBet : 100,
+	playerArray : ["Active Fund", "Passive Fund", "Dealer"],
+	playerY : 25,
+	chipValues : [1,5,10,25,50],
+	cardValues : [
+  		11,2,3,4,5,6,7,8,9,10,10,10,10, //suit 1
+  		11,2,3,4,5,6,7,8,9,10,10,10,10, //suit 2
+  		11,2,3,4,5,6,7,8,9,10,10,10,10, //suit 3
+  		11,2,3,4,5,6,7,8,9,10,10,10,10, //suit 4
+	]
+});
+
+var Flux = {
+	cardPictures : [],
+	chipPictures : [],
+	players : [],
+	graphs : [],
+	playerX : [25,500,250],
+	alive : [true, true, true],
+	hittingStatus : [true, true, true],
+	chipTotals : [1000,1000,null],
+	cardback : null,
+	felt : null,
+	chipQuintet : null,
+	handSlider : null,
+	gameOver : true,
+	newGame : true,
+	reveal : false,
+	drawBool : true,
+	handCount : 1,
+	playerInclusion : 1
+};
--- BlackJack-Final/sketch.js
+++ BlackJack-Final/sketch.js
@@ -1,129 +1,110 @@
 var sketch = function (p) {
-var cardPictures = [];
-var chipPictures = [];
-var cardValues = [
-  11,2,3,4,5,6,7,8,9,10,10,10,10, //suit 1
-  11,2,3,4,5,6,7,8,9,10,10,10,10, //suit 2
-  11,2,3,4,5,6,7,8,9,10,10,10,10, //suit 3
-  11,2,3,4,5,6,7,8,9,10,10,10,10, //suit 4
-];
-var gameOver = true, newGame = true, reveal = false, drawBool = true;
-var counter = 0, count = 0;
-var deckSize = 52, sliderMax = 10, cardSpacing = 20;
-var maxBet = 100, handCount = 1, playerInclusion = 1;
-var players = [], graphs = [];
-var hittingStatus = [true, true, true];
-var alive = [true, true, true];
-var playerArray = ["Active Fund", "Passive Fund", "Dealer"];
-var playerX = [25,500,250];
-var playerY = [25,25,25];
-var chipValues = [1,5,10,25,50];
-var chipTotals = [1000,1000,null];
-var cardback, felt, chipQuintet, handSlider;
-
+//load all pictures
 p.preload = function(){
-  for (var i = 0; i < deckSize; i++){
-    cardPictures.push(p.loadImage("assets/"+i+".png")); //import the cards
+  for (var i = 0; i < Const.deckSize; i++){
+    Flux.cardPictures.push(p.loadImage("assets/"+i+".png")); //import the cards
   }
-  for (i = 0; i < chipValues.length; i++){
-    chipPictures.push(p.loadImage("assets/Chip_"+i+".png"));  //import the chips
+  for (i = 0; i < Const.chipValues.length; i++){
+    Flux.chipPictures.push(p.loadImage("assets/Chip_"+i+".png"));  //import the chips
   }
-  cardback = p.loadImage("assets/cardback.png");
-  felt = p.loadImage("assets/green_felt.jpg");        //set the background to felt
-  chipQuintet = p.loadImage("assets/quintet.png");
+  Flux.cardback = p.loadImage("assets/cardback.png");
+  Flux.felt = p.loadImage("assets/green_felt.jpg");        //set the background to Flux.felt
+  Flux.chipQuintet = p.loadImage("assets/quintet.png");
 };
-
+//set up with p5.js functions
 p.setup = function () {
-  p.createCanvas(1920, 800);  //width of the felt image
-  handSlider = p.createSlider(1,sliderMax,1);
-  handSlider.position(playerX[0],20);
+  p.createCanvas(1920, 800);  //width of the Flux.felt image
+  Flux.handSlider = p.createSlider(1,Const.sliderMax,1);
+  Flux.handSlider.position(Flux.playerX[0],20);
   p.frameRate(1);
 
-  //change the X values of the players to match the window size
-  playerX[1] = (p.windowWidth > 900) ? p.windowWidth - 500 : 500;
-  playerX[2] = p.windowWidth / 2 - 150;
-  
+  //change the X values of the Flux.players to match the window size
+  Flux.playerX[1] = (p.windowWidth > 900) ? p.windowWidth - 500 : 500;
+  Flux.playerX[2] = p.windowWidth / 2 - 150;
+
   for (var i = 0; i <= 2; i++){
-    players[i] = new player(playerX[i],playerY[i], i, playerArray[i]);
-    graphs[i] = new graph(playerX[i], playerY[i], i);
-    players[i].takeCard(2);
-    graphs[i].setNewPoint();
+    Flux.players[i] = new player(Flux.playerX[i],Const.playerY, i, Const.playerArray[i]);
+    Flux.graphs[i] = new graph(Flux.playerX[i], Const.playerY, i);
+    Flux.players[i].takeCard(2);
+    Flux.graphs[i].setNewPoint();
   } //CPU1 i = 0, CPU2 i = 1, dealer i = 2
 };
-
+//draw loop
 p.draw = function () {
   drawBackground();
-  if (alive[0] && alive[1]){
-    //if all players are in the game
+  if (Flux.alive[0] && Flux.alive[1]){
+    //if all Flux.players are in the game
     playTheGame(0);
 
     for(var i = 0; i <=1; i++){
-    if (chipTotals[i] <= 0)
-      alive[i] = false;
+    if (Flux.chipTotals[i] <= 0)
+      Flux.alive[i] = false;
     }
   }
-  else if (!alive[0] && alive[1]){
+  else if (!Flux.alive[0] && Flux.alive[1]){
     //if CPU1 has run out of chips (is out of the game)
     playTheGame(1);//play the game without player 0
-    if (chipTotals[1] <= 0)
-      alive[1] = false;
+    if (Flux.chipTotals[1] <= 0)
+      Flux.alive[1] = false;
   }
-  else if (!alive[1] && alive[0]){
+  else if (!Flux.alive[1] && Flux.alive[0]){
     //if CPU2 has run out of chips (is out of the game)
     playTheGame(2);
-    if (chipTotals[0] <= 0)
-      alive[0] = false;
+    if (Flux.chipTotals[0] <= 0)
+      Flux.alive[0] = false;
   }
   else{
     //if both CPUs are out of chips (are out of the game)
-    playTheGame(3);//just draws the graphs
+    playTheGame(3);//just draws the Flux.graphs
   }
 };
 
 //draw simplification methods:
 drawBackground = function(){//all background elements
-  p.image(felt,0,0);
-  p.frameRate(handSlider.value());
+  p.image(Flux.felt,0,0);
+  p.frameRate(Flux.handSlider.value());
 };
+//common code between players to simplify draw loop
 playTheGame = function(whichPlayer){//all elements of the game which are constant
   for (var i = 0; i <= 1; i++)
-    graphs[i].drawGraph();
+    Flux.graphs[i].drawGraph();
 
   switch(whichPlayer){
     case 0 :
     case 1 :
       for (i = whichPlayer; i <= 2; i++)
-        players[i].drawCards();
-      
+        Flux.players[i].drawCards();
+
       for (i = whichPlayer; i <= 1; i++){
-        players[i].drawChips();
+        Flux.players[i].drawChips();
       }
-      for (i = whichPlayer; i <= playerInclusion; i++)
-        hittingStatus[i] = cpuRules(i);
-      if (!hittingStatus[0] && !hittingStatus[1] && playerInclusion == 1)
-        playerInclusion = 2;
-      if (gameOver)//start over with a new hand
+      for (i = whichPlayer; i <= Flux.playerInclusion; i++)
+        Flux.hittingStatus[i] = cpuRules(i);
+      if (!Flux.hittingStatus[0] && !Flux.hittingStatus[1] && Flux.playerInclusion == 1)
+        Flux.playerInclusion = 2;
+      if (Flux.gameOver)//start over with a new hand
         reset();
-      if (!hittingStatus[2])
+      if (!Flux.hittingStatus[2])
         stay();
       break;
     case 2 :
-      players[0].drawCards();
-      players[2].drawCards();
-      players[0].drawChips();
-      for (i = 0; i <= playerInclusion; i++)//will only activate on 0 and 2
-        hittingStatus[i] = cpuRules(i++);
-      if (!hittingStatus[0] && playerInclusion == 1)
-        playerInclusion = 2;
-      if (gameOver)
+      Flux.players[0].drawCards();
+      Flux.players[2].drawCards();
+      Flux.players[0].drawChips();
+      for (i = 0; i <= Flux.playerInclusion; i++)//will only activate on 0 and 2
+        Flux.hittingStatus[i] = cpuRules(i++);
+      if (!Flux.hittingStatus[0] && Flux.playerInclusion == 1)
+        Flux.playerInclusion = 2;
+      if (Flux.gameOver)
         reset();
-      if (!hittingStatus[2])
+      if (!Flux.hittingStatus[2])
         stay();
       break;
     default : break;
   }
 }
 
+//player function
 player = function(x, y, playerCount, type){//player object
   var hand = [], handVals = [];
   var chipHand = 0;
@@ -141,34 +122,34 @@ player = function(x, y, playerCount, type){//player object
   var chipY = betY;
   var drawing = false, winningVal = false;
   var position = 0;
-  this.alive = alive[playerCount];
+  this.alive = Flux.alive[playerCount];
   switch(type){
     case "Dealer" :
       cardY += 200;
       break;
     default : break;
   }
-  
+
 
   //card functions:
   this.takeCard = function(noCardsTaken){      //take a number of new cards
     for (var count = noCardsTaken; count >= 1; count--){
-      cardNo = Math.floor(Math.random()*cardValues.length);//pick a random card no.
-      handVals.push(cardValues[cardNo]);
-      hand.push(cardPictures[cardNo]);
+      cardNo = Math.floor(Math.random()* Const.cardValues.length);//pick a random card no.
+      handVals.push( Const.cardValues[cardNo]);
+      hand.push(Flux.cardPictures[cardNo]);
     }
   }
 
   this.drawCards = function(){      //code to draw the cards
     for (count = 0; count < hand.length; count++){ //for all cards in the hand
 
-      if (count === 0 && type == "Dealer" && !reveal){
+      if (count === 0 && type == "Dealer" && !Flux.reveal){
       //the dealer's first card is flipped over until the player stays
-        p.image(cardback, cardX + count*cardSpacing, cardY);
+        p.image(Flux.cardback, cardX + count*Const.cardSpacing, cardY);
         count++;  //move onto the next card
       }
-      
-      p.image(hand[count], cardX + count*cardSpacing, cardY);  //draw all the other cards
+
+      p.image(hand[count], cardX + count*Const.cardSpacing, cardY);  //draw all the other cards
     }
   }
 
@@ -179,8 +160,8 @@ player = function(x, y, playerCount, type){//player object
       handSum += handVals[count];
     }
     if (handSum > 21)   //check the flexible vale of the aces
-      recalculate = this.checkAces(); 
-    
+      recalculate = this.checkAces();
+
     if(recalculate){
       handSum = 0;
       for (count = 0; count < handVals.length; count++){
@@ -208,25 +189,25 @@ player = function(x, y, playerCount, type){//player object
 
   //chip functions:
   this.drawChips = function(){        //code to draw how much the player has bet
-    p.image(chipQuintet, chipX, chipY);
-    
+    p.image(Flux.chipQuintet, chipX, chipY);
+
     for (count = 0; count < chipHandPics.length; count++){
-      p.image(chipHandPics[count], betX +cardSpacing*count, betY);  //draw the current bet
+      p.image(chipHandPics[count], betX +Const.cardSpacing*count, betY);  //draw the current bet
     }
   }
 
   this.bet = function(chipType){    //code to bet a certain chip
-    if (chipHand + chipValues[chipType] <= maxBet){
-      chipHandPics.push(chipPictures[chipType]);   //add to the picture array to be printed
-      chipHand += chipValues[chipType];        //add to the integer of current bet amount
+    if (chipHand + Const.chipValues[chipType] <= Const.maxBet){
+      chipHandPics.push(Flux.chipPictures[chipType]);   //add to the picture array to be printed
+      chipHand += Const.chipValues[chipType];        //add to the integer of current bet amount
     }
   }
 
   //misc functions:
   this.winning = function(status){
     if (this.alive === true){
-      chipTotals[playerCount] += (status) ? chipHand : 0-chipHand;
-      graphs[playerCount].setNewPoint();
+      Flux.chipTotals[playerCount] += (status) ? chipHand : 0-chipHand;
+      Flux.graphs[playerCount].setNewPoint();
     }
   }
   this.handLength = function(){
@@ -238,26 +219,26 @@ player = function(x, y, playerCount, type){//player object
 };
 
 stay = function(){//code for staying:
-  reveal = true;
-  gameOver = true;
+  Flux.reveal = true;
+  Flux.gameOver = true;
 
-  handCount++;
-  dealersSum = players[2].calcHandSum();
+  Flux.handCount++;
+  dealersSum = Flux.players[2].calcHandSum();
   for (count = 0; count <= 1; count++){   //test the other player's cards
-    handSum = players[count].calcHandSum();
+    handSum = Flux.players[count].calcHandSum();
     if (handSum > 21) //if they busted, they lost
-      players[count].winning(false);
-    else if (players[count].handLength() >= 5)
-      players[count].winning(true); //if they have >= 5 cards, they win by 5 card Charlie
+      Flux.players[count].winning(false);
+    else if (Flux.players[count].handLength() >= 5)
+      Flux.players[count].winning(true); //if they have >= 5 cards, they win by 5 card Charlie
     else if (dealersSum > 21)
-      players[count].winning(true);
+      Flux.players[count].winning(true);
     else if (handSum > dealersSum && dealersSum < 21)
-      players[count].winning(true); //if they beat the dealer, they win
+      Flux.players[count].winning(true); //if they beat the dealer, they win
     else
-      players[count].winning(false);
+      Flux.players[count].winning(false);
   }
 };
-
+//graph object
 graph = function(x, y, playerNumber){//graph object
   this.x = x;
   this.y = y;
@@ -266,7 +247,7 @@ graph = function(x, y, playerNumber){//graph object
   plot.setPos(x, y);
   plot.getXAxis().setAxisLabelText("No. Hands");
   plot.getYAxis().setAxisLabelText("Total Chips (U$D)");
-  plot.setTitleText(players[playerNumber].getType());
+  plot.setTitleText(Flux.players[playerNumber].getType());
 
   //graph functions:
   this.drawGraph = function(){      //draw the graph
@@ -276,7 +257,7 @@ graph = function(x, y, playerNumber){//graph object
   this.setNewPoint = function(){//set the next point in the graph
     if (points.length >= 100)       //if there are more than 100 points
       points.shift();               //delete the first point
-    points.push(new GPoint(handCount,chipTotals[playerNumber])); 
+    points.push(new GPoint(Flux.handCount,Flux.chipTotals[playerNumber]));
     //push a new point onto the last index
     plot.setPoints(points);
   }
@@ -285,32 +266,32 @@ graph = function(x, y, playerNumber){//graph object
     plot.setPos(x,this.y);
   }
 };
-
+//function to reset the card game
 reset = function(){
-  playerX[1] = (p.windowWidth > 900) ? p.windowWidth - 500 : 500;
-  graphs[1].setX(playerX[1]);
-  playerX[2] = p.windowWidth / 2 - 150;
+  Flux.playerX[1] = (p.windowWidth > 900) ? p.windowWidth - 500 : 500;
+  Flux.graphs[1].setX(Flux.playerX[1]);
+  Flux.playerX[2] = p.windowWidth / 2 - 150;
   for (var i = 0; i <= 2; i++){
-    players[i] = new player(playerX[i],playerY[i],i, playerArray[i]);
-    players[i].takeCard(2);
-    players[i].bet(1);
-    hittingStatus[i] = true;
+    Flux.players[i] = new player(Flux.playerX[i],Const.playerY,i, Const.playerArray[i]);
+    Flux.players[i].takeCard(2);
+    Flux.players[i].bet(1);
+    Flux.hittingStatus[i] = true;
   }
-  gameOver = false;
-  reveal = false;
-  playerInclusion = 2;
+  Flux.gameOver = false;
+  Flux.reveal = false;
+  Flux.playerInclusion = 2;
 };
-
-cpuRules = function(count){              
-  var dealersCard = players[2].upSideDownCard();
-  var sum = players[count].calcHandSum();
+//function for the CPUs to decide whether to bet and how much
+cpuRules = function(count){
+  var dealersCard = Flux.players[2].upSideDownCard();
+  var sum = Flux.players[count].calcHandSum();
   if (count == 0){//active fund
     if (sum < 17){
-      players[count].bet(mapSums(sum, dealersCard));
-      seed = p.floor(Math.random()*4);
+      Flux.players[count].bet(mapSums(sum, dealersCard));
+      seed = p.floor(Math.random()*Const.humanElement);
       if (seed == 0)
-        players[count].bet(mapSums(sum, dealersCard))
-      players[count].takeCard(1);
+        Flux.players[count].bet(mapSums(sum, dealersCard))
+      Flux.players[count].takeCard(1);
       return true;
     }
   }
@@ -318,20 +299,20 @@ cpuRules = function(count){
     if (sum < 17){
       //AI for the passive fund
       switch (dealersCard){
-        case 2  : players[count].bet(2);      break;
+        case 2  : Flux.players[count].bet(2);      break;
         case 3  :
         case 4  :
         case 5  :
-        case 6  : players[count].bet(1);       break;
+        case 6  : Flux.players[count].bet(1);       break;
         case 7  :
         case 8  :
-        case 9  : 
+        case 9  :
         case 10 :
         case 11 :
         default : break;
       }
       //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      players[count].takeCard(1);
+      Flux.players[count].takeCard(1);
       return true;
     }
   }
@@ -340,7 +321,7 @@ cpuRules = function(count){
   else if (sum > 21)                   //player has busted
     return false;
 };
-
+//AI for the active firm
 mapSums = function(hand, dealer){
   var total = hand + dealer;
   //maximum is 11 + 16 = 27, minimum is 4 + 2 = 6
@@ -357,5 +338,5 @@ mapSums = function(hand, dealer){
 };
 
 };
-
+//sketch p5 object
 var myp5 = new p5(sketch);
